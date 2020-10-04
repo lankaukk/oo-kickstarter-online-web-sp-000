2a8cc4f9ea6e916c17b4e0ef45d2eceb1e580704
McKayla Lankau <lankaukk@yahoo.com>
2020-10-04 22:49:00 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 class Project
   
-  attr_accessor :name, :backed_projects
+  attr_accessor :name, :backers
 
   def initialize(name)
     @name = name
-    @backed_projects = []
+    @backers = []
   end
   
 end
